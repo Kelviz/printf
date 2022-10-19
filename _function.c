a55f4c741608b59ce36d6edacd25739858299e98
ukamaka <ukamaka.anthony01@gmail.com>
2022-10-18 17:46:49 -0700

    spacing isseus

--- _function.c
+++ _function.c
@@ -2,13 +2,9 @@
 
 /**
  * print_char - Prints a char
- *
  * @types: List a of arguments
- *
  * @buffer: Buffer array to handle print
- *
  * @flags:  Calculates active flags
- *
  * @width: Width
  *
  * @precision: Precision specification
