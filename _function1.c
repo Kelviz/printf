95cb8396d6385781d6d009a3e711f97a4286755a
ukamaka <ukamaka.anthony01@gmail.com>
2022-10-18 17:45:12 -0700

    space

--- _function1.c
+++ _function1.c
@@ -130,23 +130,14 @@ int print_hexa_upper(va_list types, char buffer[], int flags, int width, int pre
  * print_hexa - Prints a hexadecimal number in lower or upper
  *
  * @types: Lista of arguments
- *
  * @map_to: Array of values to map the number to
- *
  * @buffer: Buffer array to handle print
- *
  * @flags:  Calculates active flags
- *
  * @flag_ch: Calculates active flags
- *
  * @width: get width
- *
  * @precision: Precision specification
- *
  * @size: Size specifier
- *
  * @size: Size specification
- *
  * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size)
